14d9aad463bd9400985720b1e820668754bb9e91
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2025-04-18 16:13:42 +0530

    Add files via upload

---
+++ LeapYearOrNot.java
@@ -0,0 +1,18 @@
+package NumberSeries;
+
+public class LeapYearOrNot {
+	public static void main(String[] args) {
+		
+		int year = 2004;
+		
+		if((year % 400 == 0)|| (year % 4 == 0 && year % 100 != 0))
+		{
+			System.out.println("year is leap year ");
+		}
+		else
+		{
+			System.out.println("year is not leap year ");
+		}
+	}
+
+}
