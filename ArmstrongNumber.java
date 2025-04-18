2830c1540f7e2c73f1091dd27606987a403144ce
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2025-04-18 16:13:02 +0530

    Add files via upload

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,23 +1,28 @@
-import java.util.Scanner;
-class ArmstrongNumber{
-public static void main(String [] args){
-  Scanner sc=new Scanner(System.in);
-  System.out.println("Enter number");
-  int num=sc.nextInt();
-  int temp=num;
-  int sum=0;
-   
-  while(temp>0){
-	int rem=temp%10;
-	sum=sum+(rem*rem*rem);
-	
-	temp=temp/10;
-  }
-  if(sum==num){
-	System.out.println("Number is armstrong number");
-  }
-  else{
-	System.out.println("Number is not armstrong number");
-  }
-}
+
+package NumberSeries;
+
+public class ArmstrongNumber {
+	public static void main(String[] args) {
+		int num = 153;
+		int reminder ;
+		int result =0;
+		int temp = num;
+		
+		while(temp != 0)
+		{
+			reminder = temp % 10; // shevat cha number find karnaya sathi
+			result = result +(reminder * reminder * reminder); //cube calculate karnaya sathi
+		
+			temp = temp / 10; 		//shevat cha number udavnaya sathi 	
+		}
+		if(result == num )
+		{
+			System.out.println("Number is Armstrong ");
+		}
+		else
+		{
+			System.out.println("number is not Armstrong");
+		}
+	}
+
 }
