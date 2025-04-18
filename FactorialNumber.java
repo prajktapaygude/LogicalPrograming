bbcfc52b4e362bd49e56809d3909dbf1f53d965b
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2025-04-18 16:13:20 +0530

    Add files via upload

--- FactorialNumber.java
+++ FactorialNumber.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
-class FactorialNumber{
-public static void main(String [] args){
-  Scanner sc=new Scanner(System.in);
-  System.out.println("Enter number");
-  int num=sc.nextInt();
-  int fact=1;
-  
-  for(int i=num; i>=1; i--){
-	fact=fact*i;
-}
-System.out.print("Factorial is "+fact);
-}
+package NumberSeries;
+
+// factorial Number = 5 = 5*4*3*2*1=120
+
+public class FactorialNumber {
+	public static void main(String[] args) {
+		
+		int number = 5;
+		int fact = 1 ;
+		
+		for(int i=1 ; i<= 5 ; i++ )
+		{
+			fact = fact * i;
+		}
+		System.out.println("factorial is : "+fact);
+	}
+
 }
