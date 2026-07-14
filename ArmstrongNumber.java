d5cd8ed4007f5237737ea63f7010ab5b9b836efd
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2026-07-14 17:29:02 +0530

    Add files via upload

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,6 +1,5 @@
-
+// saglay number chi cube orignal number evdi yete 
 package NumberSeries;
-
 public class ArmstrongNumber {
 	public static void main(String[] args) {
 		int num = 153;
@@ -11,8 +10,8 @@ public class ArmstrongNumber {
 		while(temp != 0)
 		{
 			reminder = temp % 10; // shevat cha number find karnaya sathi
-			result = result +(reminder * reminder * reminder); //cube calculate karnaya sathi
-		
+			result = result +(reminder * reminder * reminder); 
+			//cube calculate karnaya sathi
 			temp = temp / 10; 		//shevat cha number udavnaya sathi 	
 		}
 		if(result == num )
