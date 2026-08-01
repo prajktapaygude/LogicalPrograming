7b75dc6062da7c2708f00ed2b40e65db27fc9262
Prajkta Paygude <145181382+prajktapaygude@users.noreply.github.com>
2026-08-01 11:24:27 +0530

    Add files via upload

--- ReverseArray.java
+++ ReverseArray.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 
 // yat jr swapping chya addhi if (arr[i] > arr[j] condition takli
 // jr array sort hotoy bubble sort ni . baki sagl same
-
 public class ReverseArray {
 	public static void main(String[] args) {
 	
@@ -12,15 +11,16 @@ public class ReverseArray {
 		for(int i = 0 ; i<=arr.length-1;i++)
 		{
 			System.out.print(arr[i]+" ");
-		}
-		
+		}		
 		System.out.println();
 		for(int i = 0 ; i<=arr.length-1; i++) {
 			for(int j = i+1; j<=arr.length-1;j++)
 			{
+				
 				int temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
+				
 			}
 		}
 		
